bfd76d1ec54869349aa90875431d12c510e9129b
Andrew Pouliot <andrewpouliot@fb.com>
2015-05-22 17:42:16 -0700

    Mac stuff

---
+++ FBTweakExampleMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FBTweakExampleMac
+//
+//  Created by Andrew Pouliot on 5/22/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
