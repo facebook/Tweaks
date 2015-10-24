227c39477552e209c47a998c7a224ff41f389ab1
Florent Vilmart <florentvilmart@me.com>
2015-10-24 19:20:01 +0200

    Adds Carthage

---
+++ Tweak/Tweaks.h
@@ -0,0 +1,44 @@
+//
+//  Tweak.h
+//  Tweak
+//
+//  Created by Florent Vilmart on 15-10-24.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tweak.
+FOUNDATION_EXPORT double TweakVersionNumber;
+
+//! Project version string for Tweak.
+FOUNDATION_EXPORT const unsigned char TweakVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tweak/PublicHeader.h>
+
+
+#import <Tweaks/FBTweak.h>
+#import <Tweaks/FBTweakEnabled.h>
+#import <Tweaks/FBTweakInline.h>
+#import <Tweaks/FBTweakInlineInternal.h>
+#import <Tweaks/FBTweakShakeWindow.h>
+#import <Tweaks/FBTweakStore.h>
+#import <Tweaks/FBTweakShakeWindow.h>
+#import <Tweaks/FBTweakCategory.h>
+#import <Tweaks/FBTweakCollection.h>
+#import <Tweaks/FBTweakViewController.h>
+
+#import <Tweaks/_FBTweakDictionaryViewController.h>
+#import <Tweaks/_FBColorComponentCell.h>
+#import <Tweaks/_FBTweakCategoryViewController.h>
+#import <Tweaks/_FBTweakColorViewController.h>
+#import <Tweaks/_FBTweakTableViewCell.h>
+#import <Tweaks/_FBTweakCollectionViewController.h>
+#import <Tweaks/_FBColorUtils.h>
+#import <Tweaks/_FBTweakColorViewControllerDataSource.h>
+#import <Tweaks/_FBTweakBindObserver.h>
+#import <Tweaks/_FBTweakColorViewControllerRGBDataSource.h>
+#import <Tweaks/_FBKeyboardManager.h>
+#import <Tweaks/_FBTweakColorViewControllerHSBDataSource.h>
+#import <Tweaks/_FBColorWheelCell.h>
+#import <Tweaks/_FBTweakArrayViewController.h>
